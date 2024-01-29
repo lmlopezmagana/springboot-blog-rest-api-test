6d5471dace949255ef1f72bfe3e8376d2fab9cca
alexluque02 <alexanderluquehoffrogge@gmail.com>
2024-01-29 20:34:13 +0100

    commit for merge

--- src/test/java/com/springboot/blog/repository/UserRepositoryTest.java
+++ src/test/java/com/springboot/blog/repository/UserRepositoryTest.java
@@ -1,11 +1,18 @@
 package com.springboot.blog.repository;
 
+import com.springboot.blog.entity.User;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 import com.springboot.blog.repository.config.ConfigTestClass;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Optional;
 
 class UserRepositoryTest extends ConfigTestClass{
+
+    @Autowired
+    UserRepository userRepository;
 
     @Test
     void findByEmail() {
@@ -13,6 +20,7 @@ class UserRepositoryTest extends ConfigTestClass{
 
     @Test
     void findByUsernameOrEmail() {
+        Optional <User> user = userRepository.findByUsernameOrEmail("")
     }
 
     @Test

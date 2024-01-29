15f5e3d4ba1a0e7d500030c28dc852e5f1b08434
Sebastián Millán <sebastian.millanordonez2019@gmail.com>
2024-01-29 12:37:36 +0100

    test findByPostId y findByUsernameOrEmail terminado

--- src/test/java/com/springboot/blog/repository/CommentRepositoryTest.java
+++ src/test/java/com/springboot/blog/repository/CommentRepositoryTest.java
@@ -1,6 +1,9 @@
 package com.springboot.blog.repository;
 
+import com.springboot.blog.entity.Comment;
+import com.springboot.blog.entity.Post;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
@@ -10,6 +13,8 @@ import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -21,6 +26,9 @@ import static org.junit.jupiter.api.Assertions.*;
 @Sql(value = "classpath:delete-test.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
 class CommentRepositoryTest {
 
+    @Autowired
+    private CommentRepository commentRepository;
+
     @Container
     @ServiceConnection
     static PostgreSQLContainer postgres = new PostgreSQLContainer(DockerImageName.parse("postgres:16-alpine"))
@@ -28,7 +36,12 @@ class CommentRepositoryTest {
             .withPassword("testSecret")
             .withDatabaseName("testDatabase");
 
+    //Sebastián Millán
     @Test
     void findByPostId() {
+        List<Comment> result = commentRepository.findByPostId(1L);
+        assertFalse(result.isEmpty());
+        assertEquals(result.size(), 3);
+        assertEquals(result.get(0).getName(), "Sebastián");
     }
 }

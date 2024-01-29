d142e29dddc28a6ece6003084d8d606038fbc757
tomasdelpinorep <tomasdelpino28@gmail.com>
2024-01-29 12:07:37 +0100

    safe merge

--- src/test/java/com/springboot/blog/comments/CommentRepositoryTest.java
+++ src/test/java/com/springboot/blog/comments/CommentRepositoryTest.java
@@ -1,4 +1,7 @@
+package com.springboot.blog.comments;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class CommentRepositoryTest {
-  
+
 }
--- src/test/java/com/springboot/blog/comments/CommentServiceImplTest.java
+++ src/test/java/com/springboot/blog/comments/CommentServiceImplTest.java
@@ -1,4 +1,35 @@
+package com.springboot.blog.comments;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class CommentServiceImplTest {
-  
+
+    @BeforeAll
+    static void init(){
+
+    }
+
+    @Test
+    void createComment() {
+    }
+
+    @Test
+    void getCommentsByPostId() {
+    }
+
+    @Test
+    void getCommentById() {
+    }
+
+    @Test
+    void updateComment() {
+    }
+
+    @Test
+    void deleteComment() {
+    }
 }

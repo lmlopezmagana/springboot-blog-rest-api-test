87f7162655bf6d449932ba5a1a980b825aab0d6d
Fran Ruiz <fjavierrule@gmail.com>
2024-01-29 12:07:44 +0100

    guardado

--- src/test/java/com/springboot/blog/service/impl/PostServiceImplTest.java
+++ src/test/java/com/springboot/blog/service/impl/PostServiceImplTest.java
@@ -1,4 +1,45 @@
+package com.springboot.blog.service.impl;
+
+import com.springboot.blog.entity.Post;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class PostServiceImplTest {
-  
+
+    @BeforeAll
+    void init (){
+
+        Post p = new Post(
+                1,
+                "Titulo 1",
+
+        );
+
+    }
+    @Test
+    void createPost() {
+    }
+
+    @Test
+    void getAllPosts() {
+    }
+
+    @Test
+    void getPostById() {
+    }
+
+    @Test
+    void updatePost() {
+    }
+
+    @Test
+    void deletePostById() {
+    }
+
+    @Test
+    void getPostsByCategory() {
+    }
 }

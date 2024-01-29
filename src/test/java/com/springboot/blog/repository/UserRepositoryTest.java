15cfd753997b8e45147304f809ec759f1af5a776
Roberto Rebolledo <robertorebolledo151@gmail.com>
2024-01-29 20:50:31 +0100

    succesfull test

--- src/test/java/com/springboot/blog/repository/UserRepositoryTest.java
+++ src/test/java/com/springboot/blog/repository/UserRepositoryTest.java
@@ -1,11 +1,20 @@
 package com.springboot.blog.repository;
 
+import com.springboot.blog.entity.User;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 import com.springboot.blog.repository.config.ConfigTestClass;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Optional;
 
 class UserRepositoryTest extends ConfigTestClass{
+
+    @Autowired
+    private UserRepository userRepository;
+
 
     @Test
     void findByEmail() {
@@ -17,6 +26,10 @@ class UserRepositoryTest extends ConfigTestClass{
 
     @Test
     void findByUsername() {
+        Optional<User> user = userRepository.findByUsername("tvenneur0");
+        Assertions.assertNotNull(user);
+        Assertions.assertEquals(user.get().getUsername(),"tvenneur0");
+
     }
 
     @Test

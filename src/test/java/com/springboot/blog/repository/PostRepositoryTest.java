b99f41812d22dc149d2248f61958963dfbf375b2
Sebastián Millán <sebastian.millanordonez2019@gmail.com>
2024-01-29 10:29:32 +0100

    config properties y test containers en repos

--- src/test/java/com/springboot/blog/repository/PostRepositoryTest.java
+++ src/test/java/com/springboot/blog/repository/PostRepositoryTest.java
@@ -1,10 +1,33 @@
 package com.springboot.blog.repository;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles({"test"})
+@Testcontainers
+@Sql(value = "classpath:insert-test.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+@Sql(value = "classpath:delete-test.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
 class PostRepositoryTest {
+
+
+    @Container
+    @ServiceConnection
+    static PostgreSQLContainer postgres = new PostgreSQLContainer(DockerImageName.parse("postgres:16-alpine"))
+            .withUsername("testUser")
+            .withPassword("testSecret")
+            .withDatabaseName("testDatabase");
 
     @Test
     void findByCategoryId() {

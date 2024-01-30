d75c92b4c3fa7bce78ecce8f4e8159a6e86c54f0
Fran Ruiz <fjavierrule@gmail.com>
2024-01-30 17:03:14 +0100

    creado by fran

---
+++ src/test/java/com/springboot/blog/repository/PostRepositoryTest.java
@@ -0,0 +1,15 @@
+package com.springboot.blog.repository;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PostRepositoryTest {
+
+    @Test
+    void findByCategoryId() {
+
+
+
+    }
+}

8dbbd3ab2ab827c12417f216843964e2e8dae283
Tomasdelpinorep <tomasdelpino28@gmail.com>
2024-01-30 22:23:21 +0100

    controller tests done

--- src/test/java/com/springboot/blog/comments/CategoryControllerTest.java
+++ src/test/java/com/springboot/blog/comments/CategoryControllerTest.java
@@ -1,11 +1,8 @@
 package com.springboot.blog.comments;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.springboot.blog.entity.Comment;
 import com.springboot.blog.entity.Post;
 import com.springboot.blog.payload.CommentDto;
-import com.springboot.blog.repository.CommentRepository;
-import com.springboot.blog.repository.PostRepository;
-import com.springboot.blog.service.CommentService;
 import com.springboot.blog.service.impl.CommentServiceImpl;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -18,14 +15,14 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+import java.util.List;
 
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -37,16 +34,20 @@ class CategoryControllerTest {
     CommentServiceImpl commentService;
     @Autowired
     ObjectMapper objectMapper;
+
+    static ModelMapper mapper = new ModelMapper();
+    static Comment comment = new Comment();
     static Post p = Mockito.mock(Post.class);
     static CommentDto c = new CommentDto();
-
-    String token;
 
     @BeforeAll
     static void init(){
         c.setName("name");
         c.setBody("body that must be of at least 10 characters");
         c.setEmail("email@email.com");
+
+        comment = mapper.map(c,Comment.class);
+        comment.setPost(p);
     }
 
 //    @BeforeEach
@@ -74,4 +75,64 @@ class CategoryControllerTest {
                 .andExpect(jsonPath("$",notNullValue()))
                 .andExpect(jsonPath("$.name",is("name")));
     }
+
+    @Test
+    @WithMockUser(username = "Tom",roles = {"USER","ADMIN"})
+    void getCommentsByPostIdTest() throws Exception {
+        when(commentService.getCommentsByPostId(anyLong())).thenReturn(List.of(c));
+
+        mvc.perform(get("/api/v1/posts/{postId}/comments",p.getId())
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$",notNullValue()))
+                .andExpect(jsonPath("$",hasSize(1)))
+                .andExpect(jsonPath("$[0].name",is("name")));
+    }
+
+    @Test
+    @WithMockUser(username = "Tom",roles = {"USER","ADMIN"})
+    void getCommentByIdTest() throws Exception{
+        when(commentService.getCommentById(anyLong(),anyLong())).thenReturn(c);
+
+        mvc.perform(get("/api/v1/posts/{postId}/comments/{commentId}", p.getId(), c.getId())
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$",notNullValue()))
+                .andExpect(jsonPath("$.name",is("name")));
+    }
+
+    @Test
+    @WithMockUser(username = "Tom",roles = {"USER","ADMIN"})
+    void updateCommentTest() throws Exception {
+        CommentDto updatedComment = new CommentDto();
+        updatedComment.setName("updated name");
+        updatedComment.setBody("updated body with more than 10 characters");
+        updatedComment.setEmail("updatedemail@email.com");
+
+        when(commentService.updateComment(anyLong(),anyLong(),Mockito.any(CommentDto.class)))
+                .thenReturn(updatedComment);
+
+        mvc.perform(put("/api/v1/posts/{postId}/comments/{commentId}", p.getId(), c.getId())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(updatedComment)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$",notNullValue()))
+                .andExpect(jsonPath("$.name",is("updated name")));
+
+    }
+
+    @Test
+    @WithMockUser(username = "Tom",roles = {"USER","ADMIN"})
+    void deleteCommentTest() throws Exception {
+        doNothing().when(commentService).deleteComment(anyLong(),anyLong());
+
+        mvc.perform(delete("/api/v1/posts/{postId}/comments/{commentId}",p.getId(),c.getId())
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Comment deleted successfully"));
+
+        verify(commentService,times(1)).deleteComment(anyLong(),anyLong());
+        verifyNoMoreInteractions(commentService);
+    }
+
 }

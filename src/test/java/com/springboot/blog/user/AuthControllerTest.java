a7b89587ff0be1be163f1cccf0b3d69c26e18149
Pedro Franch Echániz <pedrofe.av@gmail.com>
2024-01-31 21:06:33 +0100

    test controlador

--- src/test/java/com/springboot/blog/user/AuthControllerTest.java
+++ src/test/java/com/springboot/blog/user/AuthControllerTest.java
@@ -1,8 +1,10 @@
 package com.springboot.blog.user;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.springboot.blog.controller.AuthController;
 import com.springboot.blog.exception.BlogAPIException;
+import com.springboot.blog.payload.LoginDto;
 import com.springboot.blog.payload.RegisterDto;
 import com.springboot.blog.service.AuthService;
 import org.junit.jupiter.api.BeforeEach;
@@ -18,12 +20,15 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -41,6 +46,40 @@ public class AuthControllerTest {
 
     @InjectMocks
     AuthController authController;
+
+
+    @Test
+    void validLogin() throws Exception {
+        LoginDto loginDto = new LoginDto();
+        loginDto.setPassword("1234");
+        loginDto.setUsernameOrEmail("ToReshulon");
+
+        String token = "tokenGuay";
+        when(authService.login(any(LoginDto.class))).thenReturn(token);
+
+
+        mockMvc.perform(post("/api/auth/login")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(loginDto)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.accessToken").value(token));
+    }
+
+    @Test
+    void invalidLogin() throws Exception {
+        LoginDto loginDto = new LoginDto();
+        loginDto.setUsernameOrEmail("InvalidUser");
+        loginDto.setPassword("InvalidPassword");
+
+        doThrow(new BadCredentialsException("invalid login"))
+                .when(authService).login(any(LoginDto.class));
+
+        mockMvc.perform(post("/api/auth/login")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(loginDto)))
+                .andExpect(status().isUnauthorized());
+
+    }
 
     @Test
     void whenInvalidRegisterUserNameNull_thenReturns400AndErrorResult() throws Exception{
@@ -85,7 +124,7 @@ public class AuthControllerTest {
     }
 
     @Test
-    void whenEmailExists_thenThrowsException() throws Exception {
+    void whenInvalidRegisterEmailExists_thenThrowsException() throws Exception {
         RegisterDto registerDto = new RegisterDto();
         registerDto.setUsername("Pedro");
         registerDto.setEmail("user@example.com");

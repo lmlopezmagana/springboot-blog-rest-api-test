0664f1fbf7c9f426aa48c513a322f0a7ea584383
Sebastián Millán <sebastian.millanordonez2019@gmail.com>
2024-02-01 12:30:48 +0100

    empezar a testear controladores

--- src/test/java/com/springboot/blog/controller/CategoryControllerTest.java
+++ src/test/java/com/springboot/blog/controller/CategoryControllerTest.java
@@ -1,13 +1,49 @@
 package com.springboot.blog.controller;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(controllers = CategoryController.class)
 class CategoryControllerTest {
 
+    @InjectMocks
+    CategoryController categoryController;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    //Sebastián Millán
     @Test
-    void addCategory() {
+    @WithMockUser(roles = {"ADMIN"})
+    void whenNewCategoryIsValid_thenReturnHttp201() throws Exception{
+
+    }
+    //Sebastián Millán
+    @Test
+    @WithMockUser(roles = {"ADMIN"})
+    void whenNewCategoryIsInValid_thenReturnHttp401() throws Exception{
+
+    }
+    //Sebastián Millán
+    @Test
+    @WithMockUser(roles = {"ADMIN"})
+    void whenUserHasAdminRole_thenReturnHttp401() throws Exception{
+
+    }
+    //Sebastián Millán
+    @Test
+    @WithMockUser()
+    void whenUserHasUserRole_thenReturnHttp401() throws Exception{
+
     }
 
     @Test
--- src/test/java/com/springboot/blog/controller/CommentControllerTest.java
+++ src/test/java/com/springboot/blog/controller/CommentControllerTest.java
@@ -1,10 +1,24 @@
 package com.springboot.blog.controller;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(controllers = CommentController.class)
 class CommentControllerTest {
+
+    @InjectMocks
+    CommentController commentController;
+
+    @Autowired
+    MockMvc mockMvc;
 
     @Test
     void createComment() {

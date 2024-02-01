55fb60a675c469315edbeed409843c51ec66b921
Antonio Martinez <nonomartinezlopez2004@gmail.com>
2024-02-01 09:12:09 +0100

    cosas

--- src/test/java/com/springboot/blog/repository/RoleRepositoryTest.java
+++ src/test/java/com/springboot/blog/repository/RoleRepositoryTest.java
@@ -1,13 +1,21 @@
 package com.springboot.blog.repository;
 
+import com.springboot.blog.entity.Role;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 import com.springboot.blog.repository.config.ConfigTestClass;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Optional;
 
 class RoleRepositoryTest extends ConfigTestClass{
 
+    @Autowired
+    private RoleRepository roleRepository;
+
     @Test
     void findByName() {
+        Optional<Role> role = roleRepository.findByName("")
     }
 }

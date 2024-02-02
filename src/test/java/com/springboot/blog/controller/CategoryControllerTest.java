80f5dcc2beec4a4f480ec5c45bcc881a50cef393
Fernando <fernandocdd02@gmail.com>
2024-02-02 10:15:18 +0100

    para hacer checkout

--- src/test/java/com/springboot/blog/controller/CategoryControllerTest.java
+++ src/test/java/com/springboot/blog/controller/CategoryControllerTest.java
@@ -1,13 +1,35 @@
 package com.springboot.blog.controller;
 
+import com.springboot.blog.entity.Category;
+import com.springboot.blog.payload.CategoryDto;
+import com.springboot.blog.service.CategoryService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class CategoryControllerTest {
 
+    @Autowired
+    CategoryService categoryService;
+
+    @Autowired
+    MockMvc mockMvc;
+
     @Test
-    void addCategory() {
+    void addCategoryWithOKStatus() {
+        Category category1 = Category.builder()
+                .name("Category 1")
+                .description("Description 1")
+                .posts(new ArrayList<>())
+                .build();
+
     }
 
     @Test

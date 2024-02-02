7c70c6cc9ca2799851a1ef0c1845c0a529737e38
tomasdelpinorep <tomasdelpino28@gmail.com>
2024-02-02 14:21:13 +0100

    repository tests done

--- target/test-classes/import-comments.sql
+++ target/test-classes/import-comments.sql
@@ -1,5 +1,5 @@
-insert into comments (id, name, email, body) values (1, 'Creighton', 'cpfaffe0@elpais.com', 'commodo placerat praesent blandit nam nulla integer pede justo lacinia eget tincidunt eget tempus vel');
-insert into comments (id, name, email, body) values (2, 'Claybourne', 'ccorston1@admin.ch', 'amet nunc viverra dapibus nulla suscipit ligula in lacus curabitur at ipsum');
-insert into comments (id, name, email, body) values (3, 'Callie', 'cguillet2@toplist.cz', 'arcu sed augue aliquam erat volutpat in congue etiam justo etiam pretium iaculis justo in hac habitasse platea dictumst');
-insert into comments (id, name, email, body) values (4, 'Dory', 'dgrioli3@bravesites.com', 'quis tortor id nulla ultrices aliquet maecenas leo odio condimentum id luctus');
-insert into comments (id, name, email, body) values (5, 'Lorant', 'lpolo4@moonfruit.com', 'sapien placerat ante nulla justo aliquam quis turpis eget elit sodales scelerisque mauris sit amet eros');
+insert into comments (id, name, email, body, post_id) values (1, 'Creighton', 'cpfaffe0@elpais.com', 'commodo placerat praesent blandit nam nulla integer pede justo lacinia eget tincidunt eget tempus vel', 1);
+insert into comments (id, name, email, body, post_id) values (2, 'Claybourne', 'ccorston1@admin.ch', 'amet nunc viverra dapibus nulla suscipit ligula in lacus curabitur at ipsum', 1);
+insert into comments (id, name, email, body, post_id) values (3, 'Callie', 'cguillet2@toplist.cz', 'arcu sed augue aliquam erat volutpat in congue etiam justo etiam pretium iaculis justo in hac habitasse platea dictumst', 1);
+insert into comments (id, name, email, body, post_id) values (4, 'Dory', 'dgrioli3@bravesites.com', 'quis tortor id nulla ultrices aliquet maecenas leo odio condimentum id luctus', 1);
+insert into comments (id, name, email, body, post_id) values (5, 'Lorant', 'lpolo4@moonfruit.com', 'sapien placerat ante nulla justo aliquam quis turpis eget elit sodales scelerisque mauris sit amet eros', 1);

2069e1fd298d2df15f5fe8acff0c2fb3c95f2151
Sebastián Millán <sebastian.millanordonez2019@gmail.com>
2024-02-02 14:29:44 +0100

    configurar test de integracion

--- src/test/java/com/springboot/blog/controller/PostControllerIntegrationTest.java
+++ src/test/java/com/springboot/blog/controller/PostControllerIntegrationTest.java
@@ -1,10 +1,67 @@
 package com.springboot.blog.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.springboot.blog.entity.Role;
+import com.springboot.blog.entity.User;
+import com.springboot.blog.payload.PostDto;
+import com.springboot.blog.security.JwtTokenProvider;
+import com.springboot.blog.service.PostService;
+import com.springboot.blog.service.impl.PostServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("integration-test")
 class PostControllerIntegrationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private PostServiceImpl postService;
+
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+
+    @Autowired
+    private JwtTokenProvider jwtTokenProvider;
+
+    private MultiValueMap<String, String> headers;
+    private String userToken;
+    private String adminToken;
+    private Long idPost;
 
     @Test
     void createPost() {
@@ -14,8 +71,42 @@ class PostControllerIntegrationTest {
     void getAllPosts() {
     }
 
+    //Sebastián Millán
     @Test
     void getPostById() {
+    }
+
+    @BeforeEach
+    void setUp(){
+        testRestTemplate.getRestTemplate().setRequestFactory(new HttpComponentsClientHttpRequestFactory());
+        idPost=1L;
+        User user = new User(31L,"chunteu","chunteu@marketwatch.com","wK7_8hLrBj5", "Cazzie Hunte", Set.of(new Role((short)2,"user")));
+        User admin = new User(79L, "tlamblin26", "tlamblin26@vkontakte.ru", "pQ3*\"j1!>CYo#", "Teodor Lamblin", Set.of(new Role((short)1,"admin")));
+        //userToken=jwtTokenProvider.generateToken();
+        //adminToken=jwtTokenProvider.generateToken();
+        headers=new LinkedMultiValueMap<>();
+        headers.add("content-type","application/json");
+
+    }
+
+    @Test
+    @Sql("classpath:data-integration.sql")
+    void whenIdExistsAndUserRole_thenGetPostDtoAndReturn200() throws Exception{
+        //when(postService.getPostById(1L));
+        headers.add("Authorization","Bearer "+userToken);
+        ResponseEntity<PostDto> response = testRestTemplate.exchange("http://localhost/"+port+"/api/posts/"+idPost,
+                HttpMethod.GET,new HttpEntity<>(headers), PostDto.class);
+
+        assertEquals("200", response.getStatusCode());
+    }
+
+    @Test
+    //@WithMockUser()
+    void whenIdExistsAndWithoutRole_thenGetPostDtoAndReturn200() throws Exception{
+        //when(postService.getPostById(1L));
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/posts/1"))
+                .andExpect(status().isUnauthorized())
+                .andReturn();
     }
 
     @Test

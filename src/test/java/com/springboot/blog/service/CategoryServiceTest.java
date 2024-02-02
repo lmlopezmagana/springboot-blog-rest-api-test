d1432af0ba57b5d8dc6198d6412cfe44948ff6be
Fernando <fernandocdd02@gmail.com>
2024-02-02 14:27:59 +0100

    seguir con el método

--- src/test/java/com/springboot/blog/service/CategoryServiceTest.java
+++ src/test/java/com/springboot/blog/service/CategoryServiceTest.java
@@ -1,57 +1,58 @@
 package com.springboot.blog.service;
 
-import com.springboot.blog.config.ModelMapperConfig;
 import com.springboot.blog.entity.Category;
 import com.springboot.blog.payload.CategoryDto;
 import com.springboot.blog.repository.CategoryRepository;
 import com.springboot.blog.service.impl.CategoryServiceImpl;
+import jakarta.inject.Inject;
+import org.checkerframework.checker.units.qual.A;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-
-import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
 class CategoryServiceTest {
 
-    @InjectMocks
+    @Autowired
     private CategoryService categoryService;
+
+    @InjectMocks
+    private CategoryServiceImpl categoryServiceImpl;
 
     @Mock
     private CategoryRepository categoryRepository;
 
+    @Mock
+    private ModelMapper modelMapper;
+
     @Test
     void addCategory() {
-        var id = 1L;
+
+        var id = 7L;
         var name = "testName";
         var description = "testDescription";
-        CategoryDto categoryDtoExpected
-                = new CategoryDto( id, name, description);
+
         Category categoryExpected = new Category(id, name, description, null);
+
+        CategoryDto categoryDtoExpected
+                = this.modelMapper.map(categoryExpected, CategoryDto.class);
 
         Mockito.when(categoryRepository.save(Mockito.any())).thenReturn(categoryExpected);
 
-
-        CategoryDto categoryDto  = categoryService.addCategory(categoryDtoExpected);
+        CategoryDto categoryDto  = categoryServiceImpl.addCategory(categoryDtoExpected);
 
         assertEquals(categoryDto.getId(), categoryDtoExpected.getId());
         assertEquals(categoryDto.getName(), categoryDtoExpected.getName());
         assertEquals(categoryDto.getDescription(), categoryDtoExpected.getDescription());
 
-        Mockito.when(categoryRepository.findById(id)).thenReturn(Optional.of(categoryExpected));
-        Optional<Category> categoryOptional = categoryRepository.findById(id);
 
-        assertTrue(categoryOptional.isPresent());
-        assertEquals(categoryOptional.get().getName(), name);
-        assertEquals(categoryOptional.get().getDescription(), description);
 
     }
 

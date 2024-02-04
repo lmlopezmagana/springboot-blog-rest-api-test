f21eb56d170eef22dfdaa86d9ae3cca450dc39c5
Fran Ruiz <fjavierrule@gmail.com>
2024-02-04 20:31:43 +0100

    arreglar el porque no me lee la tabla categories

--- src/test/java/com/springboot/blog/repository/PostRepositoryTest.java
+++ src/test/java/com/springboot/blog/repository/PostRepositoryTest.java
@@ -1,13 +1,44 @@
 package com.springboot.blog.repository;
 
+import com.springboot.blog.entity.Post;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles({"postgres", "spring-data-jpa"})
+@Testcontainers
+@Sql(value = "classpath:insert-data.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
 class PostRepositoryTest {
 
+    @Autowired
+    PostRepository postRepository;
+
+    @Container
+    @ServiceConnection
+    static PostgreSQLContainer postgres = new PostgreSQLContainer(DockerImageName.parse("postgres:16-alpine"))
+            .withUsername("postgres")
+            .withPassword("12345678")
+            .withDatabaseName("myblog");
     @Test
     void findByCategoryId() {
+
+        List<Post> posts = postRepository.findByCategoryId(1L);
+
+        assertEquals(1, posts.size());
 
 
 
---
+++ target/classes/insert-data.sql
@@ -0,0 +1,7 @@
+insert into categories (name, description) values ('Technology', 'Posts related to technology');
+insert into categories (name, description) values ('Travel', 'Posts about different travel experiences');
+insert into categories (name, description) values ('Food', 'Delicious recipes and food adventures');
+
+insert into posts (title, description, content, category_id) values ('Introduction to Spring Boot', 'Learn the basics of Spring Boot', 'Spring Boot is a powerful framework for building Java applications.', 1);
+insert into posts (title, description, content, category_id) values ('Exploring Bali', 'Discover the beauty of Bali', 'Explore the stunning landscapes and cultural richness of Bali.', 2);
+insert into posts (title, description, content, category_id) values ('Tasty Chocolate Cake Recipe', 'A simple recipe for a delicious chocolate cake', 'Indulge in the goodness of chocolate with this easy-to-follow cake recipe.', 3);

30d626e6103856dbfd4d1dddbb3ca8748c31d91e
Sebastián Millán <sebastian.millanordonez2019@gmail.com>
2024-02-05 20:59:19 +0100

    seguir avanzando test integracion

--- src/test/java/com/springboot/blog/controller/PostControllerIntegrationTest.java
+++ src/test/java/com/springboot/blog/controller/PostControllerIntegrationTest.java
@@ -24,8 +24,10 @@ import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
@@ -43,18 +45,11 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("integration-test")
 class PostControllerIntegrationTest {
-
-    @Autowired
-    private MockMvc mockMvc;
-
     @LocalServerPort
     private int port;
 
     @Autowired
-    private ObjectMapper objectMapper;
-
-    @Autowired
-    private PostServiceImpl postService;
+    private PasswordEncoder passwordEncoder;
     @Autowired
     private AuthenticationManager authenticationManager;
     @Autowired
@@ -84,11 +79,14 @@ class PostControllerIntegrationTest {
     void setUp(){
         testRestTemplate.getRestTemplate().setRequestFactory(new HttpComponentsClientHttpRequestFactory());
         idPost=1L;
-        User user = new User(31L,"chunteu","chunteu@marketwatch.com","wK7_8hLrBj5", "Cazzie Hunte", Set.of(new Role((short)2,"user")));
-        User admin = new User(79L, "tlamblin26", "tlamblin26@vkontakte.ru", "pQ3*\"j1!>CYo#", "Teodor Lamblin", Set.of(new Role((short)1,"admin")));
-        userToken=jwtTokenProvider.generateToken(authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
-                user.getUsername(), user.getPassword())));
+        User admin = new User(1L,"Micah Eakle","meakle0","meakle0@newsvine.com", passwordEncoder.encode("kJ3(1SY6uMM"), Set.of(new Role((short)1,"ADMIN")));
+        User user = new User(51L, "Danny Girkins", "dgirkins1e", "dgirkins1e@bing.com", passwordEncoder.encode("lE2,%W?IAA"), Set.of(new Role((short)2,"USER")));
+        System.out.println(admin.getPassword());
+
+        userToken=jwtTokenProvider.generateToken(new UsernamePasswordAuthenticationToken(
+                user.getUsername(),user.getRoles()));
         //adminToken=jwtTokenProvider.generateToken();
+        System.out.println(userToken);
         headers=new LinkedMultiValueMap<>();
         headers.add("content-type","application/json");
         headers.add("Authorization","Bearer "+userToken);

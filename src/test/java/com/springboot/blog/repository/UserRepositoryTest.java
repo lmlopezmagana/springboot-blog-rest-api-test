2fa0b500f8f432fdf2096e95e46a5212c1bf121d
A.Jimenez <jimenezalejandro505@gmail.com>
2024-02-05 12:47:48 +0100

    repository test empezado

--- src/test/java/com/springboot/blog/repository/UserRepositoryTest.java
+++ src/test/java/com/springboot/blog/repository/UserRepositoryTest.java
@@ -1,7 +1,40 @@
 package com.springboot.blog.repository;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
 @DataJpaTest
+@Testcontainers
+@ActiveProfiles({"test"})
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Sql(value = "classpath:create-user.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+@Sql(value = "classpath:delete-user.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
 public class UserRepositoryTest {
+
+    @Container
+    @ServiceConnection
+    static PostgreSQLContainer postgres = new PostgreSQLContainer(DockerImageName.parse("postgres:16-alpine"))
+            .withUsername("postgres")
+            .withPassword("12345678")
+            .withDatabaseName("myblog");
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    public void existUserByUsername (){
+        Assertions.assertTrue(userRepository.existsByUsername("Nadya_User"));
+        Assertions.assertTrue(userRepository.existsByUsername("pepeillo"));
+        Assertions.assertFalse(userRepository.existsByUsername("Pepeillo"));
+    }
 }

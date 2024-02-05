f4232f3fccd3ab6b2d0c020a85ed0b1ddb6b5235
Alejandro <jimenezalejandro505@gmail.com>
2024-02-05 22:00:09 +0100

    2 tests completados

--- src/test/java/com/springboot/blog/repository/UserRepositoryTest.java
+++ src/test/java/com/springboot/blog/repository/UserRepositoryTest.java
@@ -1,17 +1,23 @@
 package com.springboot.blog.repository;
 
+import com.springboot.blog.entity.User;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.dao.IncorrectResultSizeDataAccessException;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @DataJpaTest
 @Testcontainers
@@ -36,5 +42,30 @@ public class UserRepositoryTest {
         Assertions.assertTrue(userRepository.existsByUsername("Nadya_User"));
         Assertions.assertTrue(userRepository.existsByUsername("pepeillo"));
         Assertions.assertFalse(userRepository.existsByUsername("Pepeillo"));
+        Assertions.assertFalse(userRepository.existsByUsername("usuario"));
+    }
+
+    @Test
+    public void findUserByUsernameOrEmailTest (){
+        User u = new User(1L, "Pepe", "pepeillo", "pepeillo@gmail.com", "123456789", null);
+        Assertions.assertTrue(userRepository.findByUsernameOrEmail("pepeillo", "pepeillo@gmail.com").isPresent());
+        Assertions.assertTrue(userRepository.findByUsernameOrEmail("pepeillo", "user@gmail.com").isPresent());
+        Assertions.assertEquals(u.getId(), userRepository.findByUsernameOrEmail("pepeillo", "pepeillo@gmail.com").get().getId());
+        Assertions.assertTrue(userRepository.findByUsernameOrEmail("user", "pepeillo@gmail.com").isPresent());
+        Assertions.assertFalse(userRepository.findByUsernameOrEmail("user", "user@gmail.com").isPresent());
+        Exception exception = assertThrows(IncorrectResultSizeDataAccessException.class, () ->
+                userRepository.findByUsernameOrEmail("pepeillo", "rmacmichael0@theglobeandmail.com"));
+    }
+
+    @Test
+    public void findUserByUsernameTest (){
+        User u = new User(1L, "Pepe", "pepeillo", "pepeillo@gmail.com", "123456789", null);
+        Assertions.assertTrue(userRepository.findByUsername("pepeillo").isPresent());
+        Assertions.assertTrue(userRepository.findByUsernameOrEmail("pepeillo", "user@gmail.com").isPresent());
+        Assertions.assertEquals(u.getId(), userRepository.findByUsernameOrEmail("pepeillo", "pepeillo@gmail.com").get().getId());
+        Assertions.assertTrue(userRepository.findByUsernameOrEmail("user", "pepeillo@gmail.com").isPresent());
+        Assertions.assertFalse(userRepository.findByUsernameOrEmail("user", "user@gmail.com").isPresent());
+        Exception exception = assertThrows(IncorrectResultSizeDataAccessException.class, () ->
+                userRepository.findByUsernameOrEmail("pepeillo", "rmacmichael0@theglobeandmail.com"));
     }
 }

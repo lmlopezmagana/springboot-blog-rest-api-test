40ca821f139d7bffaec237c4cb0b69f9aaac5c67
Marco <marco.pertegal.prieto@gmail.com>
2024-02-06 22:18:06 +0100

    getPostById: whenPostIdsPresentThenReturnHttp200, whenPostIdNotFoundThenReturn404

--- src/test/java/com/springboot/blog/controller/PostControllerTest.java
+++ src/test/java/com/springboot/blog/controller/PostControllerTest.java
@@ -1,10 +1,13 @@
 package com.springboot.blog.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.springboot.blog.exception.ResourceNotFoundException;
+import com.springboot.blog.payload.PostDto;
 import com.springboot.blog.service.PostService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,9 +16,10 @@ import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.hamcrest.Matchers.is;
 import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @AutoConfigureMockMvc
 @SpringBootTest
@@ -26,17 +30,24 @@ class PostControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
     @MockBean
-    private PostService categoryService;
+    private PostService postService;
     @InjectMocks
     private PostController commentController;
 
     private Long idPost;
     private Long idCategory;
 
+    private PostDto postDto;
+
     @BeforeEach
     void setUp(){
         idPost=1L;
         idCategory=1L;
+        postDto = new PostDto();
+        postDto.setId(1l);
+        postDto.setTitle("Title");
+        postDto.setDescription("Description");
+        postDto.setContent("Content");
     }
 
     @Test
@@ -48,7 +59,26 @@ class PostControllerTest {
     }
 
     @Test
-    void getPostById() {
+    void whenPostIdsPresentThenReturnHttp200() throws Exception{
+        Mockito.when(postService.getPostById(idPost)).thenReturn(postDto);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/posts/{id}", idPost)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.title", is(postDto.getTitle())));
+        Mockito.verify(postService, times(1)).getPostById(idPost);
+    }
+
+    @Test
+    void whenPostIdNotFoundThenReturn404() throws Exception{
+
+        Mockito.doThrow(new ResourceNotFoundException("Post", "id", idPost))
+                .when(postService).getPostById(idPost);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/posts/{id}", idPost)
+                        .content(objectMapper.writeValueAsString(postDto))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound());
     }
 
     @Test
@@ -62,7 +92,7 @@ class PostControllerTest {
         mockMvc.perform(MockMvcRequestBuilders.delete("/api/posts/{id}",idPost))
                 .andExpect(content().string("Post entity deleted successfully."))
                 .andExpect(status().isOk());
-        verify(categoryService, times(1)).deletePostById(idPost);
+        verify(postService, times(1)).deletePostById(idPost);
     }
 
     //Sebastián Millán
@@ -71,7 +101,7 @@ class PostControllerTest {
     void whenPostIdIsWrongAndAdminRole_thenReturnHttp400() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.delete("/api/posts/{id}","null"))
                 .andExpect(status().isBadRequest());
-        verify(categoryService, never()).deletePostById(idPost);
+        verify(postService, never()).deletePostById(idPost);
     }
 
     //Sebastián Millán
@@ -80,7 +110,7 @@ class PostControllerTest {
     void whenPostIdExistsAndUserRole_thenReturnHttp401() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.delete("/api/posts/{id}",idPost))
                 .andExpect(status().isUnauthorized());
-        verify(categoryService, never()).deletePostById(idPost);
+        verify(postService, never()).deletePostById(idPost);
 
     }
 
@@ -91,7 +121,7 @@ class PostControllerTest {
                         .contentType(MediaType.APPLICATION_JSON)
                         .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isUnauthorized());
-        verify(categoryService, never()).deletePostById(idPost);
+        verify(postService, never()).deletePostById(idPost);
 
     }
 
@@ -101,7 +131,7 @@ class PostControllerTest {
         mockMvc.perform(MockMvcRequestBuilders.get("/api/posts/category/{id}",idCategory)
                         .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk());
-        verify(categoryService, times(1)).getPostsByCategory(idCategory);
+        verify(postService, times(1)).getPostsByCategory(idCategory);
     }
     //Sebastián Millán
     @Test
@@ -109,7 +139,7 @@ class PostControllerTest {
         mockMvc.perform(MockMvcRequestBuilders.get("/api/posts/category/{id}","null")
                         .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isBadRequest());
-        verify(categoryService, never()).getPostsByCategory(idCategory);
+        verify(postService, never()).getPostsByCategory(idCategory);
     }
 
 }

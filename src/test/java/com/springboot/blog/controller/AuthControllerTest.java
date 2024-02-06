faa62a66e890b73f6eddf97661029332123426cb
Cristian <pulidocabellochristian@gmail.com>
2024-02-06 12:45:52 +0100

    Docker engine sttopped

--- src/test/java/com/springboot/blog/controller/AuthControllerTest.java
+++ src/test/java/com/springboot/blog/controller/AuthControllerTest.java
@@ -9,15 +9,20 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.http.MediaType;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
 class AuthControllerTest {
 
     @Autowired
@@ -31,13 +36,11 @@ class AuthControllerTest {
     private AuthController authController;
     private LoginDto loginDto;
     private LoginDto loginDtoEmpty;
-
     @BeforeEach
-    void setUp() {
+    void setUp(){
         loginDto = new LoginDto("username", "password");
-        loginDtoEmpty = new LoginDto("", "");
+        loginDtoEmpty = new LoginDto("","");
     }
-
     @Test
     void whenLoginIsValid_thenReturnHttp200() throws Exception {
 
@@ -46,26 +49,26 @@ class AuthControllerTest {
         Mockito.when(authService.login(Mockito.any(LoginDto.class))).thenReturn(token);
 
         mockMvc.perform(MockMvcRequestBuilders.post("/api/auth/login")
-                .content(objectMapper.writeValueAsString(loginDto))
-                .contentType(MediaType.APPLICATION_JSON)
-                .accept(MediaType.APPLICATION_JSON))
+                        .content(objectMapper.writeValueAsString(loginDto))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.accessToken").value("token"));
     }
-
     @Test
-    void whenLoginIsNull_thenReturnHttp400() throws Exception {
+    void whenLoginIsNull_thenReturnHttp400()  throws Exception{
         String token = "token";
 
         Mockito.when(authService.login(Mockito.any(LoginDto.class))).thenReturn(token);
 
         mockMvc.perform(MockMvcRequestBuilders.post("/api/auth/login")
-                .content(objectMapper.writeValueAsString(null))
-                .contentType(MediaType.APPLICATION_JSON)
-                .accept(MediaType.APPLICATION_JSON))
+                        .content(objectMapper.writeValueAsString(null))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isBadRequest());
 
     }
+
 
     @Test
     @Disabled

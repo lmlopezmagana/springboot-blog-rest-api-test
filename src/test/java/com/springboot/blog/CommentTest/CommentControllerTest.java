77ee632ea12759e74464f3db84fc8c7a5762a559
mgaangel10 <mg.aangel10@gmail.com>
2024-02-06 18:41:30 +0100

    avances

--- src/test/java/com/springboot/blog/CommentTest/CommentControllerTest.java
+++ src/test/java/com/springboot/blog/CommentTest/CommentControllerTest.java
@@ -13,19 +13,22 @@ import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MvcResult;
+import org.testcontainers.shaded.com.fasterxml.jackson.core.type.TypeReference;
 import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
 
 
+import java.util.List;
 import java.util.Set;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
@@ -42,26 +45,7 @@ public class CommentControllerTest {
     @MockBean
     private CommentService commentService;
 
-    /*@Test
-    public void testCreateComment() throws Exception {
-        long postId = 1L;
-        CommentDto commentDto = new CommentDto();
-        commentDto.setId(1L);
-        commentDto.setBody("Test comment");
 
-        when(commentService.createComment(eq(postId), any(CommentDto.class))).thenReturn(commentDto);
-
-        mockMvc.perform(post("/api/v1/posts/" + postId + "/comments")
-                        .with(user("user"))
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content("{\"name\":\"Test Name\", \"email\":\"test@email.com\", \"body\":\"Test comment\"}"))
-                .andExpect(status().isCreated())
-                .andExpect(jsonPath("$.id").value(1))
-                .andExpect(jsonPath("$.body").value("Test comment"));
-
-
-        verify(commentService, times(1)).createComment(eq(postId), any(CommentDto.class));
-    }*/
 
     @Test
     @WithMockUser(username = "Javi", roles = {"ADMIN"})
@@ -86,6 +70,99 @@ public class CommentControllerTest {
         String resultado = mvcResult.getResponse().getContentAsString();
         assertThat(resultado).isEqualToIgnoringCase(objectMapper.writeValueAsString(commentDto));
     }
+
+    @Test
+    @WithMockUser(username = "Javi", roles = {"ADMIN"})
+    void testGetByPostIdComment() throws  Exception{
+        long postId = 1L;
+        Comment comment = new Comment(1L,"hola","angel@gmail","bodyyyyyyyyyyyyy",new Post());
+        CommentDto commentDto = new CommentDto();
+        commentDto.setId(comment.getId());
+        commentDto.setName(comment.getName());
+        commentDto.setEmail(comment.getEmail());
+        commentDto.setBody(comment.getBody());
+        when(commentService.getCommentsByPostId(any(Long.class))).thenReturn(List.of(commentDto));
+
+        MvcResult mvcResult = mockMvc.perform(get("/api/v1/posts/"+postId+"/comments")
+                .contentType("application/json")
+                .accept(APPLICATION_JSON)).andExpect(status().isOk()).andReturn();
+
+        String resultado = mvcResult.getResponse().getContentAsString();
+        List<CommentDto> resultadoDto = objectMapper.readValue(resultado, new TypeReference<List<CommentDto>>(){});
+
+        assertEquals(List.of(commentDto), resultadoDto);
+
+
+    }
+    @Test
+    @WithMockUser(username = "Javi", roles = {"ADMIN"})
+    void testGetCommentById() throws Exception{
+        long postId = 1L;
+        long commentId = 1L;
+        Comment comment = new Comment(1L,"hola","angel@gmail","bodyyyyyyyyyyyyy",new Post());
+        CommentDto commentDto = new CommentDto();
+        commentDto.setId(comment.getId());
+        commentDto.setName(comment.getName());
+        commentDto.setEmail(comment.getEmail());
+        commentDto.setBody(comment.getBody());
+        when(commentService.getCommentById(any(Long.class),any(Long.class))).thenReturn(commentDto);
+        MvcResult mvcResult = mockMvc.perform(get("/api/v1/posts/"+postId+"/comments/"+commentId)
+                .contentType("application/json")
+                .accept(APPLICATION_JSON)).andExpect(status().isOk()).andReturn();
+        String resultado = mvcResult.getResponse().getContentAsString();
+        assertThat(resultado).isEqualToIgnoringCase(objectMapper.writeValueAsString(commentDto));
+
+
+    }
+
+    @Test
+    @WithMockUser(username = "Javi", roles = {"ADMIN"})
+    void testUpdateComment () throws Exception{
+        long postId = 1L;
+        long commentId = 1L;
+        Comment comment = new Comment(1L,"hola","angel@gmail","bodyyyyyyyyyyyyy",new Post());
+        CommentDto commentDto = new CommentDto();
+
+        commentDto.setName(comment.getName());
+        commentDto.setEmail(comment.getEmail());
+        commentDto.setBody(comment.getBody());
+        when(commentService.updateComment(any(Long.class),any(Long.class),any(CommentDto.class))).thenReturn(commentDto);
+
+        MvcResult mvcResult = mockMvc.perform(put("/api/v1/posts/"+postId+"/comments/"+commentId)
+                .contentType("application/json")
+                .content(objectMapper.writeValueAsString(commentDto))
+                .accept(APPLICATION_JSON)).andExpect(status().isOk()).andReturn();
+
+        String resultado = mvcResult.getResponse().getContentAsString();
+
+        assertThat(resultado).isEqualToIgnoringCase(objectMapper.writeValueAsString(commentDto));
+    }
+    @Test
+    @WithMockUser(username = "Javi", roles = {"ADMIN"})
+    void testDeleteComment() throws Exception{
+        long postId = 1L;
+        long commentId = 1L;
+        Comment comment = new Comment(1L,"hola","angel@gmail","bodyyyyyyyyyyyyy",new Post());
+        CommentDto commentDto = new CommentDto();
+        commentDto.setId(comment.getId());
+        commentDto.setName(comment.getName());
+        commentDto.setEmail(comment.getEmail());
+        commentDto.setBody(comment.getBody());
+        String mensage = "ok";
+        doNothing().when(commentService).deleteComment(any(Long.class), any(Long.class));
+
+        MvcResult mvcResult = mockMvc.perform(delete("/api/v1/posts/"+postId+"/comments/"+commentId)
+                .contentType("application/json")
+                .accept(APPLICATION_JSON)).andExpect(status().isOk()).andReturn();
+
+        String resultado = mvcResult.getResponse().getContentAsString();
+
+        verify(commentService).deleteComment(postId,commentId);
+
+    }
+
+
+
 
 
 }

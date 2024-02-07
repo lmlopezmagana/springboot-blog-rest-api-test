de2df07d78b415e5a3db61adb1b35288688e457e
alvaropol <alvaro.pold@gmail.com>
2024-02-07 10:55:55 +0100

    integration profile modified in test integration category

--- src/test/java/com/springboot/blog/Category/CategoryControllerTest.java
+++ src/test/java/com/springboot/blog/Category/CategoryControllerTest.java
@@ -10,6 +10,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.test.web.servlet.MvcResult;
@@ -25,6 +26,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles({"test"})
 class CategoryControllerTest {
 
     @Autowired

3f24191371fbeb00419c7279b9a4f5c7956992c4
mgaangel10 <mg.aangel10@gmail.com>
2024-02-07 09:05:35 +0100

    avances

---
+++ src/test/resources/import-test-comment.sql
@@ -0,0 +1,3 @@
+insert into posts (id, title, description, content, category_id) values ( 1, 'hola1', 'hola mundo java', 'hhhhhhhhhhh', 1)
+insert into posts (id, title, description, content, category_id) values ( 2, 'hola2', 'hola mundo java', 'hhhhhhhhhhh', 1)
+insert into posts (id, title, description, content, category_id) values ( 3, 'hola3', 'hola mundo java', 'hhhhhhhhhhh', 1)

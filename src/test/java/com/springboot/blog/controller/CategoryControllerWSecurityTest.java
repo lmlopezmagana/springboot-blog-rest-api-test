3fa4694d2623d7a2cc6377840d7ac72f7b8fce12
Roberto <robertorebolledo151@gmail.com>
2024-02-07 11:52:31 +0100

    borrao

--- src/test/java/com/springboot/blog/controller/CategoryControllerWSecurityTest.java
+++ src/test/java/com/springboot/blog/controller/CategoryControllerWSecurityTest.java
@@ -1,6 +1,7 @@
 package com.springboot.blog.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.springboot.blog.controller.SecurityContext.WithMockCustomUser;
 import com.springboot.blog.payload.CategoryDto;
 import com.springboot.blog.security.JwtTokenProvider;
 import com.springboot.blog.service.CategoryService;
@@ -21,7 +22,11 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import static org.hamcrest.Matchers.is;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.never;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -88,6 +93,26 @@ public class CategoryControllerWSecurityTest {
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isBadRequest());
             Mockito.verify(categoryService, never()).addCategory(categoryDto);
+
+    }
+
+    @Test
+    @WithMockUser(username = "username", roles = {"ADMIN"})
+    void updateCategory() throws Exception {
+
+        Long id = 1L;
+
+        CategoryDto newCategory = new CategoryDto(id,"Series","Lista de series");
+        Mockito.when(categoryService.updateCategory(any(),eq(id))).thenReturn(newCategory);
+
+        mockMvc.perform(MockMvcRequestBuilders.put("/api/v1/categories/{id}", id)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(newCategory))
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name",is(newCategory.getName())));
+
+
 
     }
 
